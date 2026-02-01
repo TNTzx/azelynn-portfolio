96db59c5f0c8496f6e082565287a776aaa4ae54e
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-02 09:48:40 +1100

    Add

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path/win32'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,10 @@ export default defineConfig({
       },
     }),
   ],
+
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "src"),
+    },
+  },
 })
