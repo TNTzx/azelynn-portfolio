0c2a9a2f9e704b921ed886955cb9adf8b64de892
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-02 14:28:14 +1100

    Implement animations

--- src/components/carousel/CarouselTypes.tsx
+++ src/components/carousel/CarouselTypes.tsx
@@ -1,3 +1,4 @@
+import type { Variant, Variants } from "motion";
 import type { JSX } from "react";
 
 export interface CarouselSlide {
@@ -9,4 +10,12 @@ export interface CarouselProps {
   slides: CarouselSlide[];
   currentSlide: string;
   onScreenChange?: (newSlide: CarouselSlide) => void;
+}
+
+export type CarouselDirection = -1 | 0 | 1;
+
+export interface CarouselVariants extends Variants<CarouselDirection> {
+  enter: Variant<CarouselDirection>;
+  center: Variant<CarouselDirection>;
+  exit: Variant<CarouselDirection>;
 }
