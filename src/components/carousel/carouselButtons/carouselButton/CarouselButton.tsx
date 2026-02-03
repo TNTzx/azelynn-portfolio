e26742ea9178339916adfeb39e5bfee057eb59ab
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-03 13:53:34 +1100

    Add rainbow effect

--- src/components/carousel/carouselButtons/carouselButton/CarouselButton.tsx
+++ src/components/carousel/carouselButtons/carouselButton/CarouselButton.tsx
@@ -19,20 +19,51 @@ const buttonVariants: CarouselButtonVariants = {
 const flashVariants: CarouselButtonVariants = {
   initial: {
     opacity: 0.5,
-    padding: '20px'
+    backgroundColor: 'rgba(0, 0, 0, 1)',
   },
   hover: {
     opacity: 1,
+  },
+  click: {
+    backgroundColor: [
+      'rgba(255, 255, 255, 1)',
+      'rgba(0, 0, 0, 1)',
+    ],
+
+    transition: {
+      duration: 0.25
+    }
+  }
+};
+
+const rainbowVariants: CarouselButtonVariants = {
+  initial: {
+    opacity: 0,
+  },
+  hover: {
+    opacity: 1,
+    backgroundPosition: ['0% 50%', '100% 50%'],
+    transition: {
+      duration: 0.5,
+      repeat: Infinity,
+      repeatType: 'reverse',
+      ease: 'linear'
+    }
+  },
+  click: {
+
+  }
+};
+
+const contentVariants: CarouselButtonVariants = {
+  initial: {
+    padding: '20px'
+  },
+  hover: {
     padding: '100px'
   },
   click: {
-    backgroundImage: [
-      'linear-gradient(to right, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%)',
-      'linear-gradient(to right, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%)'
-    ],
-    transition: {
-      duration: 0.25,
-    }
+
   }
 };
 
@@ -49,7 +80,7 @@ const iconVariants: CarouselButtonVariants = {
   },
   click: {
     opacity: 1,
-    rotateZ: [45, 0],
+    rotateZ: [45, 0]
   }
 };
 
@@ -66,20 +97,25 @@ export default function CarouselButton({ direction, onClick }: CarouselButtonPro
   return (
     <motion.button
       onClick={handleClick}
+      onHoverStart={() => controls.start('hover')}
+      onHoverEnd={() => controls.start('initial')}
       className="carousel__button"
       style={{
         transform: direction === 1 ? 'rotate(180deg)' : undefined,
       }}
       variants={buttonVariants}
       initial="initial"
-      whileHover="hover"
       animate={controls}
       transition={{ duration: 0.25, ease: (t) => easeOutQuint(t, 0, 1, 1) }}
     >
-      <motion.div className="carousel__button-flash" variants={flashVariants}>
+      <motion.div className="carousel__button-bg carousel__button-bg--flash" variants={flashVariants} />
+      <motion.div className="carousel__button-bg carousel__button-bg--rainbow" variants={rainbowVariants} />
+
+      <motion.div className="carousel__button-contents" variants={contentVariants}>
         <motion.p variants={iconVariants}>
           <FaArrowLeft
             style={{
+              color: 'white',
               width: '10em',
               height: '10em'
             }}
