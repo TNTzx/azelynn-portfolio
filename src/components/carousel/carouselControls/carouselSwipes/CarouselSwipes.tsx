6064ca223b4833a17553828fd01c5630058ddf15
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-04 17:19:28 +1100

    Add right direction swipe indicator

--- src/components/carousel/carouselControls/carouselSwipes/CarouselSwipes.tsx
+++ src/components/carousel/carouselControls/carouselSwipes/CarouselSwipes.tsx
@@ -1,17 +1,22 @@
+import { transform, useTransform } from 'motion/react';
 import CarouselSwipeIndicator from './carouselSwipeIndicator/CarouselSwipeIndicator';
 import './CarouselSwipes.scss';
 import type { CarouselSwipesProps } from './carouselSwipesTypes';
 
 export default function CarouselSwipes({ swipePercent }: CarouselSwipesProps) {
+  const leftSwipePercent = useTransform(swipePercent, (val) => val === null ? null : transform(val, [0, 1], [0, 1]));
+  const rightSwipePercent = useTransform(swipePercent, (val) => val === null ? null : transform(val, [-1, 0], [1, 0]));
+
+
   return (
     <div className="carousel__swipes">
       <div className="carousel__swipe-indicator-container">
-        <CarouselSwipeIndicator direction={-1} swipePercent={swipePercent} />
+        <CarouselSwipeIndicator direction={-1} swipePercent={leftSwipePercent} />
       </div>
 
-      {/* <div className="carousel__swipe-indicator-container">
-        <CarouselSwipeIndicator direction={1} swipePercent={swipePercent} isSwiping={isSwiping} />
-      </div> */}
+      <div className="carousel__swipe-indicator-container">
+        <CarouselSwipeIndicator direction={1} swipePercent={rightSwipePercent} />
+      </div>
     </div>
   );
 }
