f8dafc5c1e1ca5dac50df55a0ecd2871c56e71c6
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-06 11:24:57 +1100

    Create new hello world slide

---
+++ src/components/slides/helloWorld/HelloWorld.tsx
@@ -0,0 +1,31 @@
+import type { CarouselAnimationContext, CarouselVariants } from '@src/components';
+import './HelloWorld.scss';
+import { motion } from 'framer-motion';
+import { easeInQuint, easeOutQuint } from 'js-easing-functions';
+
+const testVariants: CarouselVariants = {
+  enter: {
+    y: "100vh"
+  },
+  center: animationContext => ({
+    rotateZ: animationContext.direction === 1 ? "90deg" : "45deg",
+    y: "0vh",
+    transition: { type: "tween", ease: (t) => easeOutQuint(t, 0, 1, 1), duration: 1 }
+  }),
+  exit: {
+    y: "100vh",
+    transition: { type: "tween", ease: (t) => easeInQuint(t, 0, 1, 1), duration: 1 }
+  }
+}
+
+export default function HelloWorld({ animationContext }: { animationContext: CarouselAnimationContext }) {
+  return (
+    <div className='slide-hello-world'>
+      <motion.div variants={testVariants} custom={animationContext}>
+        <h1 style={{ textAlign: "center" }}>Hello World</h1>
+      </motion.div>
+
+      <button onClick={() => console.log('Clicked!')}>What</button>
+    </div>
+  )
+}
