9bf8105e1d539ae4877e1c693a3246ee64c67554
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-19 12:17:19 +1100

    Implement `isActive` prop

--- src/components/carousel/carouselSlideDots/CarouselSlideDots.tsx
+++ src/components/carousel/carouselSlideDots/CarouselSlideDots.tsx
@@ -5,9 +5,13 @@ import type { CarouselSlideDotsProps } from './carouselSlideDotsTypes';
 export default function CarouselSlideDots({ slides, slideIndex }: CarouselSlideDotsProps) {
   return (
     <div className="carousel__slide-dots">
-      <CarouselSlideDot text='test1' isActive={true} />
-      <CarouselSlideDot text='test2' isActive={true} />
-      <CarouselSlideDot text='test3' isActive={true} />
+      <CarouselSlideDot text='test1' isActive={false} />
+      <CarouselSlideDot text='test2' isActive={false} />
+      <CarouselSlideDot text='test3' isActive={false} />
+      <CarouselSlideDot text='test4' isActive={false} />
+      <CarouselSlideDot text='test5' isActive={true} />
+      <CarouselSlideDot text='test6' isActive={false} />
+      <CarouselSlideDot text='test7' isActive={false} />
     </div>
   );
 }
