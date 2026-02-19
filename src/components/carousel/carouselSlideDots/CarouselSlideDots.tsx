360be1af67e9b5735ac3668c575fdf4df31ad175
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-19 15:59:12 +1100

    Add exit animation to dots

--- src/components/carousel/carouselSlideDots/CarouselSlideDots.tsx
+++ src/components/carousel/carouselSlideDots/CarouselSlideDots.tsx
@@ -1,17 +1,61 @@
+import { animationControls, motion } from 'motion/react';
 import CarouselSlideDot from './carouselSlideDot/CarouselSlideDot';
 import './CarouselSlideDots.scss';
-import type { CarouselSlideDotsProps } from './carouselSlideDotsTypes';
+import type { CarouselSlideDotsProps, CarouselSlideDotsVariants } from './carouselSlideDotsTypes';
+import { easeInBack, easeOutBack } from 'js-easing-functions';
+import { useEffect } from 'react';
 
 export default function CarouselSlideDots({ slides, activeSlideIndex }: CarouselSlideDotsProps) {
+  const controls = animationControls();
+
+  useEffect(() => {
+    let cancelled = false;
+
+    (async () => {
+      await controls.start("shown");
+      await new Promise(r => setTimeout(r, 2000));
+      if (!cancelled) {
+        await controls.start("hidden");
+      }
+    })();
+
+    return () => {
+      cancelled = true;
+    };
+  }, [controls, activeSlideIndex])
+
+  const dotsVariants: CarouselSlideDotsVariants = {
+    shown: {
+      y: "0%",
+      transition: {
+        duration: 0.5,
+        ease: (t) => easeOutBack(t, 0, 1, 1)
+      }
+    },
+    hidden: {
+      y: "100%",
+      transition: {
+        duration: 0.5,
+        ease: (t) => easeInBack(t, 0, 1, 1)
+      }
+    },
+  };
+
   return (
-    <div className="carousel__slide-dots">
-      {slides.map((slide, index) => {
-        return <CarouselSlideDot
-          key={`carouselslidedot-${slide.hash}`}
-          text={slide.displayName}
-          isActive={activeSlideIndex === index}
-        />;
-      })}
-    </div>
+    <motion.div
+      className="carousel__slide-dots-position"
+      variants={dotsVariants}
+      animate={controls}
+    >
+      <div className="carousel__slide-dots">
+        {slides.map((slide, index) => {
+          return <CarouselSlideDot
+            key={`carouselslidedot-${slide.hash}`}
+            text={slide.displayName}
+            isActive={activeSlideIndex === index}
+          />;
+        })}
+      </div>
+    </motion.div>
   );
 }
