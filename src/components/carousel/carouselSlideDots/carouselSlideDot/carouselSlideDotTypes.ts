bdfc6718e2466d37ee9e01ae95d40f71895cdce0
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-19 13:44:46 +1100

    Add animations to CarouselSlideDot

--- src/components/carousel/carouselSlideDots/carouselSlideDot/carouselSlideDotTypes.ts
+++ src/components/carousel/carouselSlideDots/carouselSlideDot/carouselSlideDotTypes.ts
@@ -1,4 +1,11 @@
+import type { Variant, Variants } from "motion/react";
+
 export interface CarouselSlideDotProps {
   text: string;
   isActive: boolean;
+}
+
+export interface CarouselSlideDotVariants extends Variants<undefined> {
+  active: Variant<undefined>;
+  inactive: Variant<undefined>;
 }
