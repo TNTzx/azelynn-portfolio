efac647ad22266d012fef9856927e736b3353e07
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-19 12:17:19 +1100

    Implement `isActive` prop

--- src/components/carousel/carouselSlideDots/carouselSlideDot/CarouselSlideDot.tsx
+++ src/components/carousel/carouselSlideDots/carouselSlideDot/CarouselSlideDot.tsx
@@ -1,11 +1,11 @@
 import './CarouselSlideDot.scss';
 import type { CarouselSlideDotProps } from './carouselSlideDotTypes';
 
-export default function CarouselSlideDot({ text, isDisabled }: CarouselSlideDotProps) {
+export default function CarouselSlideDot({ text, isActive }: CarouselSlideDotProps) {
   return (
-    <div className="carousel__slide-dot">
+    <div className="carousel__slide-dot" style={{ padding: isActive ? "0.5em 2em" : "0em" }}>
       <div className="carousel__slide-dot-counterskew">
-        {text}
+        {isActive && text}
       </div>
     </div>
   );
