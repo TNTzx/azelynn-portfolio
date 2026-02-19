6d6d7c2fceda3e172947ced47619448707aaf4ee
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-19 11:11:05 +1100

    Add positioning for slide dots

---
+++ src/components/carousel/carouselSlideDots/CarouselSlideDots.tsx
@@ -0,0 +1,10 @@
+import './CarouselSlideDots.scss';
+import type { CarouselSlideDotsProps } from './carouselSlideDotsTypes';
+
+export default function CarouselSlideDots({ slideCount, slideIndex }: CarouselSlideDotsProps) {
+  return (
+    <div className="carousel__slide-dots">
+      
+    </div>
+  );
+}
