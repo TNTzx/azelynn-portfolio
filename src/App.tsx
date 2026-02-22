e27ebd6ad543898882de6751417bf8932f67c8ab
azelynn-aretex <azelynn.aretex@gmail.com>
2026-02-23 10:18:39 +1100

    Make navigating through link pasting work

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { Navigate, Route, Routes, useLocation, useNavigate } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import { Main } from './routes'
 import './App.scss'
-import { useEffect } from 'react';
 
 export default function App() {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (location.pathname === '/' && location.hash === '') {
-      navigate('/main', { replace: true });
-    }
-  }, [location, navigate]);
-
   return (
     <Routes>
-      <Route index element={<Navigate to="/main" />} />
+      <Route path="/" element={<Navigate to="/main" replace />} />
       <Route path='/main/:slide?' element={<Main />} />
     </Routes>
   )
