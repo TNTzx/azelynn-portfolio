ba25e4b773618f87daf66414157ea3d6a1c9b884
azelynn-aretex <azelynn.aretex@gmail.com>
2026-03-05 09:43:34 +1100

    Disable swiping on `lg`

--- src/components/carousel/carouselControls/CarouselControls.tsx
+++ src/components/carousel/carouselControls/CarouselControls.tsx
@@ -2,17 +2,22 @@ import CarouselButtons from "./carouselButtons/CarouselButtons";
 import CarouselSwipes from "./carouselSwipes/CarouselSwipes";
 import type { CarouselControlsProps } from "./carouselControlsTypes";
 import './CarouselControls.scss'
+import { useViewport } from "@src/hooks";
 
 export default function CarouselControls({ swipePercent, keyPressed, switchScreen = () => {}, isLeftDisabled, isRightDisabled }: CarouselControlsProps) {
+  const viewport = useViewport();
+
   return (
     <div className="carousel__controls">
       <div className="carousel__control-container carousel__control-container--buttons">
         <CarouselButtons keyPressed={keyPressed} onClick={(direction) => switchScreen(direction)} isLeftDisabled={isLeftDisabled} isRightDisabled={isRightDisabled} />
       </div>
 
-      <div className="carousel__control-container carousel__control-container--swipes">
-        <CarouselSwipes swipePercent={swipePercent} switchScreen={switchScreen} isLeftDisabled={isLeftDisabled} isRightDisabled={isRightDisabled} />
-      </div>
+      {viewport.width < 992 && (
+        <div className="carousel__control-container carousel__control-container--swipes">
+          <CarouselSwipes swipePercent={swipePercent} switchScreen={switchScreen} isLeftDisabled={isLeftDisabled} isRightDisabled={isRightDisabled} />
+        </div>
+      )}
     </div>
   )
 }
