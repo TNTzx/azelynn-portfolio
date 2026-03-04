f883d2c5188c892985aa8867d44009ccce36c7c2
azelynn-aretex <azelynn.aretex@gmail.com>
2026-03-05 10:33:16 +1100

    Fix wrapping on `sm` screens

--- src/components/slides/nameCard/SlideNameCard.tsx
+++ src/components/slides/nameCard/SlideNameCard.tsx
@@ -24,10 +24,6 @@ function Name() {
           artist <Separator />
           storywriter
       </h4>
-
-      <h1 className="slide-name-card__name-text slide-name-card__name-text--title">
-        CONTENT<br/>CONTENT<br/>CONTENT<br/>CONTENT<br/>CONTENT<br/>CONTENT<br/>CONTENT<br/>CONTENT<br/>CONTENT<br/>CONTENT<br/>CONTENT<br/>CONTENT<br/>CONTENT<br/>
-      </h1>
     </div>
   )
 }
