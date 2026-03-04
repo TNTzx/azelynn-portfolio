44e898b1eb3f2569f34f83649af1a1e5b4131e9b
azelynn-aretex <azelynn.aretex@gmail.com>
2026-03-05 09:55:22 +1100

    Add swipe threshold

--- src/components/carousel/carouselControls/carouselSwipes/CarouselSwipes.tsx
+++ src/components/carousel/carouselControls/carouselSwipes/CarouselSwipes.tsx
@@ -3,14 +3,18 @@ import CarouselSwipeIndicator from './carouselSwipeIndicator/CarouselSwipeIndica
 import './CarouselSwipes.scss';
 import type { CarouselSwipesProps } from './carouselSwipesTypes';
 
+const SWIPE_THRESHOLD = 0.3;
+
 export default function CarouselSwipes({ swipePercent, switchScreen, isLeftDisabled, isRightDisabled }: CarouselSwipesProps) {
-  const leftSwipePercent = useTransform(swipePercent, (val) => val === null ? null : transform(val, [0, 1], [0, 1]));
-  const rightSwipePercent = useTransform(swipePercent, (val) => val === null ? null : transform(val, [-1, 0], [1, 0]));
+  const leftSwipePercent = useTransform(swipePercent, (val) => val === null ? null : transform(val, [0, SWIPE_THRESHOLD, 1], [0, 0, 1]));
+  const rightSwipePercent = useTransform(swipePercent, (val) => val === null ? null : transform(val, [-1, -SWIPE_THRESHOLD, 0], [1, 0, 0]));
 
   const persistSwipePercent = useMotionValue(0);
 
   useMotionValueEvent(swipePercent, 'change', latestValue => {
     if (latestValue === null) {
+      if (Math.abs(persistSwipePercent.get()) < SWIPE_THRESHOLD) return;
+
       const persistLatestValue = persistSwipePercent.get();
       switchScreen?.(persistLatestValue < 0 ? 1 : -1)
       return;
