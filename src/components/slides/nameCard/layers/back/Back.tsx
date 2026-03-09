87fcd265d4ae67b09e14eeeba4c927e7c4462c8b
azelynn-aretex <azelynn.aretex@gmail.com>
2026-03-09 12:35:21 +1100

    Add fade-in to background

--- src/components/slides/nameCard/layers/back/Back.tsx
+++ src/components/slides/nameCard/layers/back/Back.tsx
@@ -1,10 +1,28 @@
 import { ImageBeach } from "@src/assets/images";
 import './Back.scss'
+import type { CarouselVariants } from "@src/components/carousel";
+import { motion } from "motion/react";
 
 export default function Back() {
+  const variants: CarouselVariants = {
+    enter: {
+      opacity: 0
+    },
+    center: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: 1
+      }
+    },
+    exit: {
+      opacity: 0
+    }
+  }
+
   return (
     <div className="slide-name-card__back">
-      <img src={ImageBeach} className="slide-name-card__image-beach-element" />
+      <motion.img src={ImageBeach} variants={variants} className="slide-name-card__image-beach-element" />
     </div>
   )
 }
