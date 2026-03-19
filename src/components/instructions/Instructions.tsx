6ac6826269d9317af579847dba81ebe047c45805
azelynn-aretex <azelynn.aretex@gmail.com>
2026-03-19 15:11:42 +1100

    Add animations

--- src/components/instructions/Instructions.tsx
+++ src/components/instructions/Instructions.tsx
@@ -2,27 +2,78 @@ import { useState } from "react";
 import "./Instructions.scss";
 import type { InstructionsProps } from "./InstructionsTypes";
 import { FaChevronDown } from "react-icons/fa";
+import type { CarouselVariants } from "../carousel";
+import { AnimatePresence, motion } from "motion/react";
+import { EASEOUTBACK, EASEOUTQUINT } from "@src/utils";
 
 export default function Instructions(props: InstructionsProps) {
   const [isShown, setIsShown] = useState(true);
 
-  return <>
-    {isShown &&
-      <div className="instructions">
-        <div className="instructions__controls">
-          <button className="instructions__close-button">
+  function toggleIsShown() {
+    setIsShown(prev => !prev);
+  }
+
+
+  const mainVariants: CarouselVariants = {
+    enter: {},
+    center: {
+      transition: { delayChildren: 2 }
+    },
+    exit: {
+    }
+  };
+
+  const innerVariants: CarouselVariants = {
+    enter: {
+      y: "100%",
+    },
+    center: {
+      y: "0%",
+      transition: {
+        duration: 0.5,
+        ease: EASEOUTBACK
+      }
+    },
+    exit: {
+      y: "100%",
+      transition: {
+        duration: 0.5,
+        ease: EASEOUTQUINT
+      }
+    }
+  };
+
+  return (
+    <motion.div variants={mainVariants} className="instructions">
+      <motion.div variants={innerVariants} className="instructions__inner">
+        <motion.div className="instructions__controls">
+          <button onClick={toggleIsShown} className="instructions__close-button">
             <FaChevronDown style={{ width: 20, height: 20 }}/>
           </button>
-        </div>
-        <div className="instructions__content">
-          <h3
-            className="instructions__text"
-            data-desktop={props.desktop}
-            data-tablet={props.tablet}
-            data-mobile={props.mobile}
-          />
-        </div>
-      </div>
-    }
-  </>;
+        </motion.div>
+        <AnimatePresence>
+          {isShown &&
+            <motion.div
+              initial={{ height: 0 }}
+              animate={{ height: "auto", transition: { duration: 0.5, ease: EASEOUTBACK } }}
+              exit={{ height: 0, transition: { duration: 0.5, ease: EASEOUTQUINT } }}
+              className="instructions__content-wrapper"
+            >
+              <motion.div
+                onClick={toggleIsShown}
+                className="instructions__content"
+              >
+                <h3
+                  className="instructions__text"
+                  data-desktop={props.desktop}
+                  data-tablet={props.tablet}
+                  data-mobile={props.mobile}
+                />
+              </motion.div>
+            </motion.div>
+          }
+        </AnimatePresence>
+      </motion.div>
+    </motion.div>
+  );
 }
