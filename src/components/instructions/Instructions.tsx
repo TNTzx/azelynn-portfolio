78f9c81c6d635e3050afed09a3a33627c889a3b3
azelynn-aretex <azelynn.aretex@gmail.com>
2026-03-19 11:34:45 +1100

    Add button

--- src/components/instructions/Instructions.tsx
+++ src/components/instructions/Instructions.tsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
 import "./Instructions.scss";
 import type { InstructionsProps } from "./InstructionsTypes";
+import { FaChevronDown } from "react-icons/fa";
 
 export default function Instructions(props: InstructionsProps) {
   const [isShown, setIsShown] = useState(true);
 
-  return (
-    <div className="instructions">
-      <div className="instructions__inner">
-        <h3
-          className="instructions__text"
-          data-desktop={props.desktop}
-          data-tablet={props.tablet}
-          data-mobile={props.mobile}
-        />
+  return <>
+    {isShown &&
+      <div className="instructions">
+        <div className="instructions__controls">
+          <button className="instructions__close-button">
+            <FaChevronDown style={{ width: 20, height: 20 }}/>
+          </button>
+        </div>
+        <div className="instructions__content">
+          <h3
+            className="instructions__text"
+            data-desktop={props.desktop}
+            data-tablet={props.tablet}
+            data-mobile={props.mobile}
+          />
+        </div>
       </div>
-    </div>
-  );
+    }
+  </>;
 }
